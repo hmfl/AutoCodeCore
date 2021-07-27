7748c2d858f47575fd240740af684584e9f93ca7
hmfl <hmfl@yeah.net>
2021-07-27 10:15:27 +0800

    提交版本控制

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using System;
+
+namespace AutoCode
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string[] s = new string[]{"1",""};
+             var s1 = s.Contains("1");
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
